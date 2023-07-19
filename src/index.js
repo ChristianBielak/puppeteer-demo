ec720b2036e1e11d584350de55f5db789cfc00e2
Christian Bielak <christian.bielak-extern@deutschebahn.com>
2023-07-19 14:57:41 +0200

    running text

--- src/index.js
+++ src/index.js
@@ -11,5 +11,5 @@ app.get("/getProducts", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
